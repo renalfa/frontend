473c2be4866a2d7621ca2ac7dc22342cb461cba8
rnlfzi <rfaidzilah@gmail.com>
2025-04-14 11:19:37 +0700

    [CHANGE] change to js

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <Button>Test</Button>
+    </div>
+  );
+};
+
+export default Home;
